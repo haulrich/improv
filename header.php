d5970e880d9aafe6d83179a4e4bf62044427f5bf
Heidi Ulrich <haulrich@users.noreply.github.com>
2022-01-17 21:51:02 +0100

    Update all files

--- header.php
+++ header.php
@@ -21,32 +21,11 @@
         }
     </script>
     <script async defer crossorigin="anonymous" src="https://connect.facebook.net/nl_NL/sdk.js#xfbml=1&version=v7.0&appId=1538696539739752&autoLogAppEvents=1" nonce="osBQol7B"></script>
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-160037129-2"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'UA-160037129-2');
-    </script>
-    <!-- Google Tag Manager -->
-    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-KWC6WPG');</script>
-    <!-- End Google Tag Manager -->
     <?php dynamic_sidebar('scripts-header'); ?>
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
 <?php dynamic_sidebar('scripts-bodystart'); ?>
-
-<!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KWC6WPG"
-                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
 
 <header class="sticky-top">
     <div class="d-flex flex-md-column">
